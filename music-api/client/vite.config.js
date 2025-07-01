acd980567a8f3b870929f30de22629d4c2dfcf14
mwaura mwangi <fmwauramwangi@gmail.com>
2025-07-01 11:33:38 +0300

    fix vite config and add vercel rewrites

--- music-api/client/vite.config.js
+++ music-api/client/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/", // this is important for Vercel deployments
   plugins: [react()],
   server: {
     host: true,
